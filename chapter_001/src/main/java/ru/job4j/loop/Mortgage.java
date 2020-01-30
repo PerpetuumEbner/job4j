0db2d7b887cbbf05573cea11c24e6a63c564b7b3
Stanislav Yulin <bataleonundisputed@gmail.com>
2020-01-30 21:53:41 +0500

    Ипотека.[#225910] (update)

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,12 +3,11 @@ package ru.job4j.loop;
 public class Mortgage {
     public int year(int amount, int salary, double percent) {
         int year = 0;
-        double debt = amount + percent / 100 * amount;
+        double debt = amount;
         while (debt > 0) {
-            debt = debt - salary;
+            debt = debt + percent / 100 * debt - salary;
             year++;
         }
         return year;
-
     }
 }
