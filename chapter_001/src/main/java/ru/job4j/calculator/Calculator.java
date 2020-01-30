d482c9feea084923fad0701b4108cb587e1c70f2
Stanislav Yulin <bataleonundisputed@gmail.com>
2020-01-30 23:08:32 +0500

    JavaDoc. Code convention[#225916]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculator;
 
+/**
+ * Package for calculate task.
+ *
+ * @author Stanislav Yulin
+ * @version 1.0
+ * @since 30.01.2020
+ */
 public class Calculator {
+    /**
+     * Math operations
+     * @param first First argement
+     * @param second Second argument
+     */
     public static void add(int first, int second) {
         int result = first + second;
         System.out.println(first + " + " + second + " = " + result);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Stanislav Yulin
+ * @version 1.0
+ * @since 1
+ */
+
+package ru.job4j.calculator;
