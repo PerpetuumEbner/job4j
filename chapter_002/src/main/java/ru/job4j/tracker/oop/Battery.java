3e6c4a6328b5f7c82288836d0f602edf18d5cf60
PerpetuumEbner <yustas@protonmail.com>
2020-02-22 01:14:01 +0500

    Состояние объекта.[#226011]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery (int charge) {
+        this.load = charge;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery fist = new Battery(100);
+        Battery second = new Battery(65);
+        System.out.println(fist.load + " : " + second.load);
+        fist.exchange(second);
+        System.out.println(fist.load + " : " + second.load);
+    }
+}
