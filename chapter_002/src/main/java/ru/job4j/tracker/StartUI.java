04d28ae70e78f37962e2f4e0b2d64fc4565798af
PerpetuumEbner <yustas@protonmail.com>
2020-03-08 22:16:03 +0500

    Реализация класса StartUI.[#225984]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,72 @@
+package ru.job4j.tracker;
+
+import java.util.Scanner;
+
+public class StartUI {
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            this.showMenu();
+            System.out.println();
+            System.out.print("Select: ");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("Add new Item");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+            } else if (select == 1) {
+                System.out.println("Show all items");
+                tracker.findAll();
+            } else if (select == 2) {
+                System.out.println("Edit item");
+                System.out.println("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                System.out.println("Enter id :");
+                String id = scanner.nextLine();
+                if (tracker.replace(id, item)) {
+                    System.out.println("successfully");
+                } else System.out.println("error");
+            } else if (select == 3) {
+                System.out.println("Delete item");
+                System.out.println("Enter id :");
+                String id = scanner.nextLine();
+                if (tracker.delete(id)) {
+                    System.out.println("successfully");
+                } else System.out.println("error");
+            } else if (select == 4) {
+                System.out.println("Find item by Id");
+                System.out.println("Enter id :");
+                String id = scanner.nextLine();
+                tracker.findById(id);
+            } else if (select == 5) {
+                System.out.println("Find items by name");
+                String name = scanner.nextLine();
+                tracker.findByName(name);
+            } else if (select == 6) {
+                System.out.println("Exit Program");
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        System.out.println("Menu.");
+        System.out.println("0. Add new Item");
+        System.out.println("1. Show all items");
+        System.out.println("2. Edit item");
+        System.out.println("3. Delete item");
+        System.out.println("4. Find item by Id");
+        System.out.println("5. Find items by name");
+        System.out.println("6. Exit Program");
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+    }
+}
