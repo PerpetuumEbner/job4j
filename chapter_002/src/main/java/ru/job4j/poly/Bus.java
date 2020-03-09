65aadf4df26219472a506d91cbb1e1cdddb3053b
PerpetuumEbner <yustas@protonmail.com>
2020-03-09 16:34:34 +0500

    Интерфейс Input.[#225975]

--- chapter_002/src/main/java/ru/job4j/poly/Bus.java
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -3,12 +3,12 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void ride() {
-
+        System.out.println("Поехали");
     }
 
     @Override
     public void passengers(int amount) {
-
+        System.out.println("Список пассажиров");
     }
 
     @Override
