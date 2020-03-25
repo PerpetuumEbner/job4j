bc812701f81fd26861656cc6f82f0321153c339b
PerpetuumEbner <yustas@protonmail.com>
2020-03-26 00:20:18 +0500

    Рефакторинг теста @Before @After.[#225971]

--- chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.stragery;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -9,35 +11,42 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    private PrintStream stdout = System.out;
+    private ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(new PrintStream(stdout));
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         Paint.draw(new Square());
-        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+        assertThat(out.toString(), is(new StringBuilder()
                 .append("++++").append(System.lineSeparator())
                 .append("+  +").append(System.lineSeparator())
                 .append("+  +").append(System.lineSeparator())
                 .append("++++").append(System.lineSeparator())
                 .append(System.lineSeparator())
                 .toString()));
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         Paint.draw(new Triangle());
-        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+        assertThat(out.toString(), is(new StringBuilder()
                 .append("   +   ").append(System.lineSeparator())
                 .append("  +++  ").append(System.lineSeparator())
                 .append(" +++++ ").append(System.lineSeparator())
                 .append("+++++++").append(System.lineSeparator())
                 .append(System.lineSeparator())
                 .toString()));
-        System.setOut(stdout);
     }
 }
