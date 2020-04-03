a47156fa4edd3e9b026995e8e14ef9693ff72053
PerpetuumEbner <yustas@protonmail.com>
2020-04-03 23:44:59 +0500

    Тестирование исключений с junit.[#225993]

--- chapter_002/src/test/java/ru/job4j/ex/FactTest.java
+++ chapter_002/src/test/java/ru/job4j/ex/FactTest.java
@@ -2,15 +2,10 @@ package ru.job4j.ex;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
 public class FactTest {
     @Test(expected = IllegalArgumentException.class)
     public void whenFact() {
         Fact fact = new Fact();
-        int expect = 720;
-        int result = fact.calc(6);
-        assertThat(result, is(expect));
+        fact.calc(-1);
     }
 }
