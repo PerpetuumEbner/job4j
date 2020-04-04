e41a28626f9caa1cb19500c116095cb84309362e
PerpetuumEbner <yustas@protonmail.com>
2020-04-05 00:48:21 +0500

    Тесты на ValidateInput.[#225997]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ValidateInputTest {
+    @Test
+    public void whenInvalidInput() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = System.out;
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        String[] data = {"one", "1"};
+        ValidateInput input = new ValidateStubInput(data);
+        input.askInt("Enter");
+        assertThat(
+                byteArrayOutputStream.toString(),
+                is(String.format("Please enter validate data again.%n"))
+        );
+        System.setOut(printStream);
+    }
+
+    @Test
+    public void whenMaxInput() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = System.out;
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        String[] data = {"5", "1"};
+        ValidateInput input = new ValidateStubInput(data);
+        input.askInt("Enter", data.length);
+        assertThat(
+                byteArrayOutputStream.toString(),
+                is(String.format("Please select key from menu.%n"))
+        );
+        System.setOut(printStream);
+    }
+}
