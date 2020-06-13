2881a1b6f5ee09c4b11d91e049605c0a64e8324f
PerpetuumEbner <yustas@protonmail.com>
2020-06-13 23:18:03 +0500

    Сортировка User с использованием Comparator.

---
+++ chapter_003/src/test/java/ru/job4j/collection/JobComparatorTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+
+public class JobComparatorTest {
+    @Test
+    public void whenJobNameDecrease() {
+        Comparator<Job> comNameDec = new JobNameDecrease();
+        int rsl = comNameDec.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenJobNameIncrease() {
+        Comparator<Job> comNameInc = new JobNameIncrease();
+        int rsl = comNameInc.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenJobPriorityDecrease() {
+        Comparator<Job> comPriorityDec = new JobPriorityDecrease();
+        int rsl = comPriorityDec.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenJobPriorityIncrease() {
+        Comparator<Job> comPriorityInc = new JobPriorityIncrease();
+        int rsl = comPriorityInc.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+}
