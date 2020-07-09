03131d4984ab11a3523a47f39520250600a53a77
PerpetuumEbner <yustas@protonmail.com>
2020-07-09 21:44:10 +0500

    Уникальность элементов и сортировка. 2.

--- chapter_004/src/main/java/ru/job4j/stream/CutCloneAddress.java
+++ chapter_004/src/main/java/ru/job4j/stream/CutCloneAddress.java
@@ -1,12 +1,13 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CutCloneAddress {
     public List<Address> collect(List<Address> address) {
-        SortedAddress sortedAddress = new SortedAddress();
-        address.sort(sortedAddress);
+        Comparator<Address> addressComparator = Comparator.comparing(Address::getCity);
+        address.sort(addressComparator);
         return address.stream().distinct().collect(Collectors.toList());
     }
 }
