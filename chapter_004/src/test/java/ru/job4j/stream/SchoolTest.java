22612c47973e3922b6a147a1cb4ffb062a9e3858
PerpetuumEbner <yustas@protonmail.com>
2020-07-11 20:51:42 +0500

    Преобразование List в Map.

--- chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -3,7 +3,9 @@ package ru.job4j.stream;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
@@ -65,6 +67,54 @@ public class SchoolTest {
 
                 new Student("Светлана", 84),
                 new Student("Владислав", 95));
+
+        assertThat(expected, is(result));
+    }
+
+    @Test
+    public void whenConvertListToMap() {
+        List<Student> students = Arrays.asList(
+                new Student("Антон", 64),
+                new Student("Владимир", 15),
+                new Student("Светлана", 84),
+                new Student("Василий", 6),
+                new Student("Мария", 57),
+                new Student("Владислав", 95));
+
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("Антон", 64);
+        expected.put("Владимир", 15);
+        expected.put("Светлана", 84);
+        expected.put("Василий", 6);
+        expected.put("Мария", 57);
+        expected.put("Владислав", 95);
+
+        Map<String, Integer> result = new School().convertListToMap(students);
+
+        assertThat(expected, is(result));
+    }
+
+    @Test
+    public void whenThereAreDuplicatesInTheList() {
+        List<Student> students = Arrays.asList(
+                new Student("Антон", 64),
+                new Student("Владимир", 15),
+                new Student("Светлана", 84),
+                new Student("Василий", 6),
+                new Student("Мария", 57),
+                new Student("Владимир", 15),
+                new Student("Светлана", 84),
+                new Student("Владислав", 95));
+
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("Антон", 64);
+        expected.put("Владимир", 15);
+        expected.put("Светлана", 84);
+        expected.put("Василий", 6);
+        expected.put("Мария", 57);
+        expected.put("Владислав", 95);
+
+        Map<String, Integer> result = new School().convertListToMap(students);
 
         assertThat(expected, is(result));
     }
