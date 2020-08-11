ff94ca175607468fbc912b570513d1d0a2b92d5a
PerpetuumEbner <yustas@protonmail.com>
2020-08-11 23:29:42 +0500

    flatMap для итератора. 2.

--- chapter_004/src/main/java/ru/job4j/stream/FlatIt.java
+++ chapter_004/src/main/java/ru/job4j/stream/FlatIt.java
@@ -1,16 +1,15 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FlatIt {
     public static List<Integer> flatten(Iterator<Iterator<Integer>> iterator) {
-        List<Integer> list = new ArrayList<>();
-        while (iterator.hasNext()) {
-            list.stream().flatMap(it -> it.getClass()).collect(Collectors.toList());
-        }
-        return list;
+        return Stream.iterate(iterator, x -> iterator.hasNext(), x -> x)
+                .flatMap(it -> Stream.iterate(iterator, x -> iterator.hasNext(), x -> x))
+                .map(Iterator::next)
+                .collect(Collectors.toList());
     }
 }
