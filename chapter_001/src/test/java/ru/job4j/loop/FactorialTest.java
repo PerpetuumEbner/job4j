dcf27fae4c23b12682b8e629ebc4262e1e4ece23
Stanislav <yustas@protonmail.com>
2021-09-27 23:24:57 +0500

    Убраны ненужные комментарии.

--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertThat;
 public class FactorialTest {
     @Test
     public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
-        //напишите здесь тест, проверяющий, что факториал для числа 5 равен 120.
         int rsl = Factorial.calc(5);
         int expected = 120;
         assertThat(rsl, is(expected));
@@ -16,7 +15,6 @@ public class FactorialTest {
 
     @Test
     public void whenCalculateFactorialForZeroThenOne() {
-        //напишите здесь тест, проверяющий, что факториал для числа 0 равен 1.
         int rsl = Factorial.calc(0);
         int expected = 1;
         assertThat(rsl, is(expected));
