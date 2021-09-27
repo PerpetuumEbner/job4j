2e597cd1d50a882e4392c860d1ba3f7fb2545c66
Stanislav <yustas@protonmail.com>
2021-09-27 16:52:49 +0500

    Build fix.

--- chapter_002/db/ insert.sql
+++
@@ -1,7 +0,0 @@
-insert into items(name)
-values ('Add new Item'),
-       ('Edit item'),
-       ('Delete item'),
-       ('Find item by Id'),
-       ('Find items by name'),
-       ('Exit Program');
