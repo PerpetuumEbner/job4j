1de51c5b9a2cb2322758c7b773bdcc6d30962078
Stanislav <yustas@protonmail.com>
2021-10-05 16:24:31 +0500

    1.4.4.3.6. Math.abs. 3.

--- chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
+++ chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
@@ -8,7 +8,7 @@ public class ChessBoard {
         if (x1 >= 0 && x1 <= 7 && x2 >= 0 && x2 <= 7
                 && y1 >= 0 && y1 <= 7 && y2 >= 0 && y2 <= 7) {
             count = Math.abs(x2 - x1);
-            if (Math.abs(x2 - x1) == count && Math.abs(y2 - y1) == count) {
+            if (Math.abs(y2 - y1) == count) {
                 rsl = count;
             }
         }
