96a332f041af0600790d0e67c1a50cb8f1545166
Stanislav <yustas@protonmail.com>
2021-10-07 14:14:39 +0500

    1.5.5.0.1. Сумма чисел. 2.

--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -9,16 +9,16 @@ public class CounterTest {
 
     @Test
     public void whenStart0Finish10() {
-        assertThat(Counter.sum(0, 10), is(10));
+        assertThat(Counter.sum(0, 10), is(55));
     }
 
     @Test
     public void whenStart3Finish8() {
-        assertThat(Counter.sum(3, 8), is(5));
+        assertThat(Counter.sum(3, 8), is(33));
     }
 
     @Test
     public void whenStart1Finish1() {
-        assertThat(Counter.sum(1, 1), is(0));
+        assertThat(Counter.sum(1, 1), is(1));
     }
 }
